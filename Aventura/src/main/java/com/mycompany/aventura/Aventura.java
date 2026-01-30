be81b0684a55519df4506dfdf6ae01e9f248c9e3
svetlanaGorban1 <sgorban01@educantabria.es>
2026-01-30 17:58:14 +0100

    Primer commit

---
+++ Aventura/src/main/java/com/mycompany/aventura/Aventura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aventura;
+
+/**
+ *
+ * @author damt110
+ */
+public class Aventura {
+
+    public static void main(String[] args) {
+       
+    }
+}
